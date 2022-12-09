0e2d5c85cc307c72e86ca128d1b6a97409222ba1
defox <defoxyweex@gmail.com>
2022-12-09 21:29:07 +0300

    Выполненное задание 3

--- src/ru/mirea/task3/CircleTest.java
+++ src/ru/mirea/task3/CircleTest.java
@@ -1,8 +1,12 @@
 package ru.mirea.task3;
 
+import java.util.Scanner;
+
 public class CircleTest {
     public static void main(String[] args) {
-        Circle circle = new Circle(10);
+        System.out.print("Введите радиус круга >> ");
+        Scanner scanner = new Scanner(System.in);
+        Circle circle = new Circle(scanner.nextInt());
         System.out.println("Circle\nSquare: " + circle.getSquare() + "\nCircumference length: " + circle.getCircumferenceLength() + "\nRadius: " + circle.getRadius());
         System.out.println("Circle raidus = 5");
         circle.setRadius(5);
--- src/ru/mirea/task3/HumanTest.java
+++ src/ru/mirea/task3/HumanTest.java
@@ -1,8 +1,15 @@
 package ru.mirea.task3;
 
+import java.util.Scanner;
+
 public class HumanTest {
     public static void main(String[] args) {
-        Human human = new Human(new Head("Brown"), new Leg[]{new Leg(100), new Leg(100)},new Hand[]{new Hand(50), new Hand(50)});
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите цвет волос, длину ног, длину рук");
+        String head = scanner.next();
+        int legSize = scanner.nextInt();
+        int handSize = scanner.nextInt();
+        Human human = new Human(new Head(head), new Leg[]{new Leg(legSize), new Leg(legSize)},new Hand[]{new Hand(handSize), new Hand(handSize)});
         System.out.println(human.toString());
     }
 }
