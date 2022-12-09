1e923e3376dd77076b5f633fdabfbea2476b89c0
defox <defoxyweex@gmail.com>
2022-12-09 21:25:03 +0300

    Выполненное задание 3

--- src/ru/mirea/task3/BookTest.java
+++ src/ru/mirea/task3/BookTest.java
@@ -1,16 +1,24 @@
 package ru.mirea.task3;
 
+import java.util.Scanner;
+
 public class BookTest {
     public static void main(String[] args) {
         Book book = new Book();
         System.out.println(book.toString());
 
-        book.setAuthor("Дюма");
-        System.out.println(book.getAuthor());
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите автора >> ");
+        book.setAuthor(scanner.next());
 
-        book.setTitle("Граф Монте-Кристо");
-        book.setPages((short)1715);
-        book.setColor("Красный");
+        System.out.print("Введите название >> ");
+        book.setTitle(scanner.next());
+
+        System.out.print("Введите количество страниц >> ");
+        book.setPages(scanner.nextShort());
+
+        System.out.print("Введите цвет >> ");
+        book.setColor(scanner.next());
 
         System.out.println(book.toString());
     }
