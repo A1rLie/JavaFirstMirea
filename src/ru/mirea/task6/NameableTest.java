d63995e489339967acb5829ef1f4d8bef428dd33
defox <defoxyweex@gmail.com>
2022-12-09 21:42:17 +0300

    Выполненное задание 6

--- src/ru/mirea/task6/NameableTest.java
+++ src/ru/mirea/task6/NameableTest.java
@@ -1,9 +1,13 @@
 package ru.mirea.task6;
 
+import java.util.Scanner;
+
 public class NameableTest {
 
     public static void main(String[] args) {
-        Planet planet = new Planet("Сатурн");
+        System.out.print("Введи название планеты >> ");
+        Scanner scanner = new Scanner(System.in);
+        Planet planet = new Planet(scanner.next());
         System.out.println("Мое имя: " + planet.getName());
     }
 }
--- src/ru/mirea/task6/PriceableTest.java
+++ src/ru/mirea/task6/PriceableTest.java
@@ -1,8 +1,12 @@
 package ru.mirea.task6;
 
+import java.util.Scanner;
+
 public class PriceableTest {
     public static void main(String[] args) {
-        Product product = new Product("Машина", 1_234_567);
+        System.out.print("Введи название машины >> ");
+        Scanner scanner = new Scanner(System.in);
+        Product product = new Product(scanner.next(), 1_234_567);
         System.out.println(product.getType() + " price: " + product.getPrice());
     }
 }
