4c57ce1c4cad0dac741126c1f354db11e170bf80
defox <defoxyweex@gmail.com>
2022-12-09 21:37:44 +0300

    Выполненное задание 5

--- src/ru/mirea/task5/DogTest.java
+++ src/ru/mirea/task5/DogTest.java
@@ -1,12 +1,17 @@
 package ru.mirea.task5;
 
+import java.util.Scanner;
+
 public class DogTest {
 
     public static void main(String[] args) {
-        Korgi korgi = new Korgi("Tomas Shelbi", "black", 0.5, 0.9);
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Кличка, цвет >> ");
+        Korgi korgi = new Korgi(scanner.next(), scanner.next(), 0.5, 0.9);
         korgi.Bark();
 
-        SibaInu sibaInu = new SibaInu("Jon Travolta", "yellow", 0.7, 0.8);
+        System.out.print("Кличка, цвет >> ");
+        SibaInu sibaInu = new SibaInu(scanner.next(), scanner.next(), 0.7, 0.8);
         sibaInu.Bark();
     }
 }
--- src/ru/mirea/task5/FurnitureTest.java
+++ src/ru/mirea/task5/FurnitureTest.java
@@ -1,8 +1,12 @@
 package ru.mirea.task5;
 
+import java.util.Scanner;
+
 public class FurnitureTest {
     public static void main(String[] args) {
-        Sofa sofa = new Sofa("белый");
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Цвет дивана >> ");
+        Sofa sofa = new Sofa(scanner.next());
         sofa.Chill();
     }
 }
